0912478e6c7a8f476b9465dda95c998c18e8ccef
math-vrn <vnikitin90@gmail.com>
2020-07-27 15:55:33 -0500

    replace C++ CUDA code by cupy functions

--- tomostream/recon.py
+++ tomostream/recon.py
@@ -1,5 +1,4 @@
 import pvaccess as pva
-# from orthorec import *
 import numpy as np
 import time
 
@@ -8,11 +7,9 @@ from  tomostream import log
 from  tomostream import pv
 from  tomostream import solver
 
-
 def readByChoice(ch):
     allch = ch.get('')['value']
     return allch['choices'][allch['index']]
-
 
 def streaming(args):
     """
@@ -89,7 +86,6 @@ def streaming(args):
             return
 
         nonlocal numProj
-
         curId = pv['uniqueId']
         frameTypeAll = ts_pvs['chStreamFrameType'].get('')['value']
         frameType = frameTypeAll['choices'][frameTypeAll['index']]
@@ -119,9 +115,7 @@ def streaming(args):
     chFlatDark.monitor(addFlatDark, '')
 
     # create solver class on GPU
-    slv = solver.OrthoRec(bufferSize, width, height, numThetaPi)
-    # allocate memory for result slices
-    recAll = np.zeros([width, 3*width], dtype='float32')
+    slv = solver.Recon(bufferSize, width, height, numThetaPi)
 
     # wait until dark and flats are acquired
     while(flgFlatDark == False):
@@ -131,14 +125,14 @@ def streaming(args):
     projBuffer[:] = flatBuffer[0]
 
     # copy mean dark and flat to GPU
-    slv.setFlat(np.mean(flatBuffer, axis=0))
-    slv.setDark(np.mean(darkBuffer, axis=0))
+    slv.setFlat(flatBuffer)
+    slv.setDark(darkBuffer)
 
     ##### streaming reconstruction ######
     while(1):
         if(readByChoice(ts_pvs['chStreamStatus']) == 'Off'):
             continue
-        # with mrwlock.r_locked():  # lock buffer before reading
+        
         projPart = projBuffer.copy()
         thetaPart = thetaBuffer.copy()
 
@@ -157,16 +151,11 @@ def streaming(args):
 
         # reconstruct on GPU
         util.tic()
-        recX, recY, recZ = slv.recOrtho(
-            projPart, thetaPart*np.pi/180, center, idX, idY, idZ)
+        rec = slv.recon(projPart, thetaPart, center, idX, idY, idZ)
         log.info('rec time:', util.toc())
 
-        # concatenate (supposing nz<n)
-        recAll[:height, :width] = recX
-        recAll[:height, width:2*width] = recY
-        recAll[:, 2*width:] = recZ
         # write to pv
-        pvRec['value'] = ({'floatValue': recAll.flatten()},)
+        pvRec['value'] = ({'floatValue': rec.flatten()},)
         # reconstruction rate limit
         time.sleep(0.1)
 
