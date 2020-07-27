5f2cd42d2034cf865f857fa4272e1649188d360c
Viktor Nikitin <vnikitin@anl.gov>
2020-07-27 16:04:43 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tomostream',
     version=open('VERSION').read().strip(),
-    author='Viktor Nikitin',
+    author='Viktor Nikitin, Francesco De Carlo',
     url='https://github.com/xray-imaging/tomostream',
     packages=find_packages(),
     include_package_data = True,
