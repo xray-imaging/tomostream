35656c274458f0d7dadaf0ec8b2451e1fd6de386
Viktor Nikitin <vnikitin@anl.gov>
2020-08-10 08:49:36 -0500

    remove StartScan PV

--- tomostream/pv.py
+++ tomostream/pv.py
@@ -22,7 +22,6 @@ def init(tomoscan_prefix):
     ts_pvs['chStreamNumDarkFields'] = pva.Channel(tomoscan_prefix + 'NumDarkFields', pva.CA)
     ts_pvs['chStreamNumFlatFields'] = pva.Channel(tomoscan_prefix + 'NumFlatFields', pva.CA)
 
-    ts_pvs['chStartScan'] = pva.Channel(tomoscan_prefix + 'StartScan', pva.CA)    
     ts_pvs['chStreamStatus'] = pva.Channel(tomoscan_prefix + 'StreamStatus', pva.CA)
     ts_pvs['chStreamBufferSize'] = pva.Channel(tomoscan_prefix + 'StreamBufferSize', pva.CA)
     ts_pvs['chStreamRingRemoval'] = pva.Channel(tomoscan_prefix + 'StreamRingRemoval', pva.CA)
