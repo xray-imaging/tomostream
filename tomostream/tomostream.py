3a29b256c69ac1e4d9eb542923b3e44d3755b12d
Francesco De Carlo <decarlof@gmail.com>
2021-01-11 18:25:36 -0600

    added check that tomoScan is running

--- tomostream/tomostream.py
+++ tomostream/tomostream.py
@@ -28,6 +28,8 @@ class TomoStream():
 
     def __init__(self, pv_files, macros):
 
+        log.setup_custom_logger("./tomostream.log")
+
         # init pvs
         self.config_pvs = {}
         self.control_pvs = {}
@@ -49,6 +51,10 @@ class TomoStream():
         self.epics_pvs['RotationStep']       = PV(prefix + 'RotationStep')
         
         self.epics_pvs['PSOPVPrefix']        = PV(prefix + 'PSOPVPrefix')
+        if self.epics_pvs['PSOPVPrefix'].get(as_string=True) == None:
+            log.error("TomoScan is down")
+            log.error("Type exit() here and start TomoScan first")
+            return
         self.epics_pvs['ThetaArray']         = PV(self.epics_pvs['PSOPVPrefix'].get(as_string=True) + 'motorPos.AVAL')
     
         # pva type channel for flat and dark fields pv broadcasted from the detector machine
@@ -87,7 +93,6 @@ class TomoStream():
         # Start the watchdog timer thread
         thread = threading.Thread(target=self.reset_watchdog, args=(), daemon=True)
         thread.start()
-        log.setup_custom_logger("./tomostream.log")
         
     def pv_callback(self, pvname=None, value=None, char_value=None, **kw):
         """Callback function that is called by pyEpics when certain EPICS PVs are changed
