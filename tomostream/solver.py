fix array broadcasting

tomostream/solver.py
@@ -35,12 +35,13 @@ class Solver():
         
         #CPU storage for the buffer
         self.data = np.zeros([ntheta, nz, n], dtype=datatype)
+        self.theta = np.zeros([ntheta], dtype='float32')
         # GPU storage for dark and flat fields
         self.dark = cp.array(cp.zeros([nz, n]), dtype='float32')
         self.flat = cp.array(cp.ones([nz, n]), dtype='float32')
         # GPU storages for ortho-slices, and angles        
         self.obj = cp.zeros([n, 3*n], dtype='float32')# ortho-slices are concatenated to one 2D array
-        self.theta = cp.zeros([ntheta], dtype='float32')
+        
         
         # reconstruction parameters 
         self.idx = np.int32(idx)
@@ -55,7 +56,7 @@ class Solver():
 
         # calculate chunk size fo gpu
         mem = cp.cuda.Device().mem_info[1]
-        self.chunk = min(self.ntheta,int(np.ceil(mem/self.n/self.nz/8)))
+        self.chunk = min(self.ntheta,int(np.ceil(mem/self.n/self.nz/32)))#cuda raw kernels do not work with huge sizes (issue in cupy?)
         print(f'chunk size {self.chunk}')
 
         # flag controlling appearance of new dark and flat fields   
@@ -144,12 +145,12 @@ class Solver():
         """Reconstruction with splitting data by chunks processed on GPU"""
     
         obj = cp.zeros([self.n, 3*self.n], dtype='float32')# ortho-slices are concatenated to one 2D array                
-        nchunks = np.ceil(data.shape[0]/self.chunk)
+        nchunks = int(np.ceil(data.shape[0]/self.chunk))
         for ichunk in range(nchunks):
-            data_gpu = cp.array(data[ichunk*self.chunk:min(ichunk*self.chunk,data.shape[0])]).astype('float32')
-            theta_gpu = theta[ichunk*self.chunk:min(ichunk*self.chunk,data.shape[0])].astype('float32')
+            data_gpu = cp.array(data[ichunk*self.chunk:min((ichunk+1)*self.chunk,data.shape[0])]).astype('float32')            
+            theta_gpu = cp.array(theta[ichunk*self.chunk:min((ichunk+1)*self.chunk,data.shape[0])]).astype('float32')
             obj += self.recon(data_gpu,theta_gpu)
-        
+            
         return obj
         
     def recon_optimized(self, data, theta, ids, center, idx, idy, idz, rotx, roty, rotz, fbpfilter, dezinger, dbg=False):
@@ -200,7 +201,6 @@ class Solver():
         if(recompute_part):            
             # subtract old part
             self.obj -= self.recon_by_chunks(self.data[ids], self.theta[ids])    
-
         # update data in the buffer
         self.data[ids] = data.reshape(data.shape[0], self.nz, self.n)
         self.theta[ids] = theta

tomostream/tomostream.py
@@ -165,9 +165,9 @@ class TomoStream():
         else:        
             dtheta = self.theta[1]-self.theta[0]
             buffer_size = np.where(self.theta-self.theta[0]>180-dtheta)[0][0]
-        if(buffer_size*width*height>pow(2,32)):
-            log.error('buffer_size %s not enough memory', buffer_size)
-            exit(0)        
+        # if(buffer_size*width*height>pow(2,32)):
+        #     log.error('buffer_size %s not enough memory', buffer_size)
+        #     exit(0)        
         # queue
         self.data_queue = queue.Queue(maxsize=buffer_size)
         
@@ -211,8 +211,9 @@ class TomoStream():
         self.pva_flat.monitor(self.add_flat,'')        
         # start monitoring projection data        
         self.pva_plugin_image.monitor(self.add_data,'')
-        # start monitoring projection data                
-        self.pva_theta.monitor(self.reinit_monitors,'')
+        # start monitoring theta
+        self.pva_theta.monitor(self.add_theta,'')
+        self.update_theta = False
         self.stream_is_running = True
 
     def add_data(self, pv):
@@ -255,6 +256,11 @@ class TomoStream():
             print('Norm flat', np.linalg.norm(data))
             log.error('new flat fields acquired')
     
+    def add_theta(self,pv):
+        """Notify about theta update"""
+        
+        self.update_theta = True
+
     def begin_stream(self):
         """Run streaming reconstruction by sending new incoming projections from the queue to the solver class,
         and broadcasting the reconstruction result to a pv variable
@@ -281,8 +287,10 @@ class TomoStream():
             while ((not self.data_queue.empty()) and (nitem < self.buffer_size)):
                 item = self.data_queue.get()
                 # reinit if data sizes were updated (e.g. after data binning by ROI1)
-                if(len(item['projection'])!=self.width*self.height):
+                if(len(item['projection'])!=self.width*self.height) or (self.update_theta):
                     self.reinit_monitors()
+                    nitem = 0
+                    continue
 
                 self.proj_buffer[nitem] = item['projection']
                 self.theta_buffer[nitem] = item['theta']